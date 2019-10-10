343976ee3129c7d0329660d15791bf14d9dac727
Bruce Taylor <bruce.taylor@avios.com>
2019-10-10 14:56:46 +0100

    Added rollup config for JS compiling and resolving.

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+// rollup.config.js
+import json from 'rollup-plugin-json';
+import replace from 'rollup-plugin-replace';
+// import resolve from 'rollup-plugin-node-resolve';
+// import commonjs from 'rollup-plugin-commonjs';
+
+const pkg = require('./package.json');
+const name = pkg.name;
+const version = pkg.version;
+
+// Used for serviceworker.js in this instance
+const plugins = [
+    json(),
+    replace({
+        delimiters: ['{{', '}}'],
+        name,
+        version
+    })
+    // resolve(),
+    // commonjs(),
+];
+
+export default [
+    {
+        // Me trying to dynamically update the service worker version number
+        // I don't think this is the right solution though.
+        // I may set up the logic in a Nunjucks serviceworker file
+        input: './src/serviceworker.js',
+        output: [
+            {
+                dir: 'dist',
+                format: 'cjs',
+            }
+        ],
+        plugins: plugins
+    }
+];
