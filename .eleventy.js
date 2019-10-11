5026a0c1a72b0c8498be7198679aa106f6517bc1
Bruce Taylor <bruce.taylor@avios.com>
2019-10-11 18:42:33 +0100

    Cleaning up directory structure.

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,19 @@
 module.exports = function(eleventyConfig) {
 
-    // eleventyConfig.addPassthroughCopy("dist/**/*");
+    eleventyConfig.addPassthroughCopy({ "dist/static": "static" });
+    eleventyConfig.addPassthroughCopy({ "dist/styles": "styles" });
+    eleventyConfig.addPassthroughCopy({ "dist/robots.txt": "robots.txt" });
+    eleventyConfig.addPassthroughCopy({ "dist/sitemap.xml": "sitemap.xml" });
+    eleventyConfig.addPassthroughCopy({ "dist/serviceworker.js": "serviceworker.js" });
 
     return {
         passthroughFileCopy: true,
         markdownTemplateEngine: "njk",
-        templateFormats: ["html", "njk", "md"],
+        templateFormats: [
+            "html",
+            "njk",
+            "md"
+        ],
         dir: {
             input: "src",
             output: "_site",
