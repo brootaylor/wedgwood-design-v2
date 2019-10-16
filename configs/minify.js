7cfdcb0ad9be49386f17ced848e4b50687826ac3
Bruce Taylor <bruce.taylor@avios.com>
2019-10-16 15:58:38 +0100

    Updated the minify config file.

--- configs/minify.js
+++ configs/minify.js
@@ -1,14 +1,5 @@
 const htmlmin = require('html-minifier');
 
-/**
- * Convert a string of text to Markdown
- * https://medium.com/confrere/e735c3f2f45e
- *
- * @param {String} content Rendered content
- * @param {String} outputPath Path to rendered file
- * @return {String} Minified HTML
- *
- */
 module.exports = (content, outputPath) => {
   if (outputPath && outputPath.endsWith('.html')) {
     const minified = htmlmin.minify(content, {
