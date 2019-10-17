79f49182ac01c216f8e2e8b07ac6ffffc4fcaedf
Bruce Taylor <bruce.taylor@avios.com>
2019-10-17 13:45:44 +0100

    Added some data config.

---
+++ src/data/app.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: 'Wedgwood Design',
+    short_title: 'WD',
+    description: 'Wedgwood Design company website',
+    url: process.env.URL || '',
+    theme_color: '#212227',
+    accent_color: '#20e',
+    background_color: '#fff',
+    favicon: '/images/app.ico',
+    icon: '/images/app.png',
+    author: {
+      name: 'Bruce Taylor',
+      url: 'https://brootaylor.com'
+    },
+};
