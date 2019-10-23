ada12d60af5cc78bb0520d6970192c1f15f8b712
Bruce Taylor <bruce.taylor@avios.com>
2019-10-23 16:26:43 +0100

    Lots of config updates.

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function(eleventy) {
 
 	// BrowserSync
-	eleventy.setBrowserSyncConfig(require('./configs/browser-sync.config.js'));
+	eleventy.setBrowserSyncConfig(require('./config/browser-sync.config.js'));
 
 	// Plugins
 	eleventy.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'));
 
 	// Transforms
-	eleventy.addTransform('minify', require('./configs/minify.js'));
+	eleventy.addTransform('minify', require('./config/minify.js'));
 
 	// Passthrough
 	eleventy.addPassthroughCopy('./src/assets/ico');
